feb99f90dc27563637a4c85aefa2fbe19a0413bc
justalinko <alinkokomansuby@gmail.com>
2021-10-16 17:53:34 +0700

    add: command init

--- dev/command/Init.php
+++ dev/command/Init.php
@@ -11,23 +11,41 @@ class Init extends Command
 {
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'app:init';
-    protected static $defaultDescription = 'Run local php-server development mode';
+    protected static $defaultDescription = 'Setting environment variables';
     protected function configure(): void
     {
        
-        $this->addOption('at' , null, InputOption::VALUE_OPTIONAL , 'Default server is http://localhost:8888 you can change address with options --at');
+        $this->addOption('base-url' , null, InputOption::VALUE_OPTIONAL , 'Setting the base_url configuration');
+        $this->addOption('database' , null, InputOption::VALUE_OPTIONAL , 'Setting the database configuration --database=host,user,pass,dbname');
+
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): void
     {
-       if($input->getOption('at') == null)
-       {
-           @system('php -S localhost:8888 -t public/');
-           Command::SUCCESS;
-       }else{
-            @system('php -S '.$input->getOption('at').' -t public/');
-           Command::SUCCESS;
+      $base = $input->getOption('base-url');
+      $db = $input->getOption('database');
+      $this->configurenv($base,$db);
 
-       }
+      return Command::SUCCESS;
+      
+    }
+    protected function set($env,$change,$content)
+    {
+        return preg_replace("/".$env."?=?(.*)?/","{$env} = {$change}" , $content);
+    }
+    protected function configurenv($baseurl,$db)
+    {
+        if($baseurl == null){ $baseurl = "http://localhost:8888"; }
+        if($db == null ){ $db = "localhost,root,root,reikodb";}
+
+        if(file_exists('env.example'))
+        {
+            @copy('env.example' , '.env');
+        }
+        $cfg = file_get_contents('.env');
+        $newconfig = $this->set('APP_BASEURL' , $baseurl,$cfg);
+    
+        file_put_contents('.env',$newconfig);
+        
     }
 }
