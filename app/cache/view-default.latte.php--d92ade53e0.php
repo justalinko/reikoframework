08558c18adf03eb1a36587c7885d7834e84396fc
Rei Yan <hariyantoriyan.hr@gmail.com>
2021-10-16 20:47:35 +0700

    fix: change landing page

--- app/cache/view-default.latte.php--d92ade53e0.php
+++ app/cache/view-default.latte.php--d92ade53e0.php
@@ -9,17 +9,7 @@ final class Templated92ade53e0 extends Latte\Runtime\Template
 	public function main(): array
 	{
 		extract($this->params);
-		echo '<?php
-$logoASCII = "
-                   _ __       
-        ________  (_) /______ 
-       / ___/ _ \\/ / //_/ __ \\
-      / /  /  __/ / ,< / /_/ /
-     /_/   \\___/_/_/|_|\\____/ 
-     ";
-
-?>
-<!DOCTYPE html>
+		echo '<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -27,45 +17,25 @@ $logoASCII = "
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Reiko Framework</title>
     <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;300;400;600&display=swap" rel="stylesheet">
+    <style>
+        * {
+            font-family: \'Nunito\', sans-serif;
+        }
+    </style>
 </head>
 <body>
     <div id="app">
         <div class="flex h-screen">
-            <div class="m-auto">
-                <p class="pb-5 text-xl font-semibold">REIKO FRAMEWORK</p>
-                <div class="rounded-lg border grid grid-cols-2 gap-5 p-5">
-                    <div class="w-96 border-b border-r rounded-lg p-5">
-                        <p class="text-lg font-medium">Home</p>
-                        <p class="py-2">Cupcake ipsum dolor sit amet sweet. I love toffee muffin I love sweet caramels jelly. Chupa chups I love jujubes powder marshmallow dessert tootsie roll carrot cake I love.</p>
-                        <a href="#" class="flex flex-row items-center justify-items-center gap-x-3 text-indigo-500 pointer appearance-none cursor-pointer">
-                            Read More
-                            <svg class="w-5 h-5 stroke-current" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                        </a>
-                    </div>
-                    <div class="w-96 border-b border-l rounded-lg p-5">
-                        <p class="text-lg font-medium">Documentation</p>
-                        <p class="py-2">Cupcake ipsum dolor sit amet sweet. I love toffee muffin I love sweet caramels jelly. Chupa chups I love jujubes powder marshmallow dessert tootsie roll carrot cake I love.</p>
-                        <a href="#" class="flex flex-row items-center justify-items-center gap-x-3 text-indigo-500 pointer appearance-none cursor-pointer">
-                            Read More
-                            <svg class="w-5 h-5 stroke-current" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                        </a>
-                    </div>
-                    <div class="w-96 border-t border-r rounded-lg p-5">
-                        <p class="text-lg font-medium">Contribute</p>
-                        <p class="py-2">Cupcake ipsum dolor sit amet sweet. I love toffee muffin I love sweet caramels jelly. Chupa chups I love jujubes powder marshmallow dessert tootsie roll carrot cake I love.</p>
-                        <a href="#" class="flex flex-row items-center justify-items-center gap-x-3 text-indigo-500 pointer appearance-none cursor-pointer">
-                            Read More
-                            <svg class="w-5 h-5 stroke-current" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                        </a>
-                    </div>
-                    <div class="w-96 border-t border-l rounded-lg p-5">
-                        <p class="text-lg font-medium">Donate</p>
-                        <p class="py-2">Cupcake ipsum dolor sit amet sweet. I love toffee muffin I love sweet caramels jelly. Chupa chups I love jujubes powder marshmallow dessert tootsie roll carrot cake I love.</p>
-                        <a href="#" class="flex flex-row items-center justify-items-center gap-x-3 text-indigo-500 pointer appearance-none cursor-pointer">
-                            Read More
-                            <svg class="w-5 h-5 stroke-current" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3"></path></svg>
-                        </a>
-                    </div>
+            <div class="m-auto flex flex-col items-center">
+                <p class="pb-5 text-6xl font-extralight text-center">REIKO FRAMEWORK</p>
+                <div class="flex flex-row gap-x-7">
+                    <a href="#">Home</a>
+                    <a href="#">Documentation</a>
+                    <a href="#">Contribute</a>
+                    <a href="#">Donate</a>
                 </div>
             </div>
         </div>
